97d77510986536ea2bf8039a483b86068986e1cb
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-26 19:38:12 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a[100000],n,i,j,k,m=1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<a[i+1])
+        {
+        m++;
+        }
+    }
+    if(n==m)
+    printf("yes");
+    else
+    printf("no");
+}
+getch();
